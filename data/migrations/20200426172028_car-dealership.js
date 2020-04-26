a0df696920e7cde0acf99dbc8eb7c5f2b875d362
nuvallo <nuvallo.inc@gmail.com>
2020-04-26 17:25:14 -0400

    database table created

--- data/migrations/20200426172028_car-dealership.js
+++ data/migrations/20200426172028_car-dealership.js
@@ -1,3 +1,21 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (table) => {
+    table.increments();
 
-exports.down = function (knex) {};
+    table.string("VIN").notNullable().unique().index();
+
+    table.string("make").notNullable();
+
+    table.string("model").notNullable();
+
+    table.string("mileage").notNullable();
+
+    table.string("transmission");
+
+    table.boolean("title");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
